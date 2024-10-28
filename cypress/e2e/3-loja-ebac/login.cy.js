7fe6162a184338e7c5320f3742d61a17024c8f50
Daniella Maleski <daniella.maleski@gmail.com>
2024-10-28 18:49:46 -0300

    arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 ///<reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -32,4 +33,20 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail dani.teste@teste.com.br está incorreta.')
         cy.get('.woocommerce-error').should('exist')
     });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, dani.teste')
+    })
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => { //utilizando funções nativas do Cypress
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false}) // log false serve para esconder o dado no video de execuçao do cypress
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, dani.teste')
+        })
+    })
 })
